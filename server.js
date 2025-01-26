dcdf4a021b48a898b94a8e8c530e59f0cca5ccc0
aisehiwebwala <116726737+aisehiwebwala@users.noreply.github.com>
2025-01-27 00:32:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 app.use(express.static(path.join(__dirname,"client","dist")));
 app.get("/api",(req,res)=>{res.send("Hello World!!!")});
 app.get("/dir",(req,res)=>{
-    res.send(fs.readdirSync(__dirname+"/client"));
+    res.send(req.query.dirr);
 })
 app.use("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"client","dist","index.html"));
